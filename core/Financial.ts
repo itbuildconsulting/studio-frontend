0195c3aba946a20614483dc10dfea69724858142
Richard Salles <richard.danielcs@gmail.com>
2024-12-05 22:36:19 -0300

    FEAT: Adicionando API na página de financeiro

---
+++ core/Financial.ts
@@ -0,0 +1,52 @@
+import Cookies from 'js-cookie';
+
+async function conectAPI(req: object | null, url: string, method: string) {
+    const token = Cookies.get('admin-user-sci-auth');
+    let config = {};
+
+    if (req === null) {
+        config = {
+            method,
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': `Bearer ${token}`
+            },
+        };
+    } else {
+        config = {
+            method,
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify(req),
+        };
+    }
+
+    try {
+        const resp: any = await fetch(
+            `${process.env.NEXT_PUBLIC_SERVER_URL_API}${url}`,
+            config
+        );
+
+        if (resp.status === 201 || resp.status === 200) {
+            const authResp: any = await resp.json();
+
+            return authResp;
+        } else {
+            const error = await resp.json();
+            throw new Error(JSON.stringify(error));
+        }
+    } catch (error) {
+        return error;
+    }
+}
+
+export default class FinancialRepository implements FinancialRepository {
+    async getLatestTransactions(): Promise<[]> {
+        const req: any = null;
+        return conectAPI(req, "/financial/lastTransactions", "GET");
+    }
+
+
+}
---
+++ core/FinancialRepository.ts
@@ -0,0 +1,6 @@
+import Financial from "./Financial";
+
+export default interface FinancialRepository {
+
+    getLatestTransactions(): Promise<Financial[]>;
+}
