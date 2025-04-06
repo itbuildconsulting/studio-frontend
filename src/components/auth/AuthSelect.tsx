843eddce4adc94f86000de5a0a01fab5c54d8929
Richard Salles <richard.danielcs@gmail.com>
2025-04-05 21:43:28 -0300

    FIX: Resolvendo problemas de deploy

--- src/components/auth/AuthSelect.tsx
+++ src/components/auth/AuthSelect.tsx
@@ -48,9 +48,10 @@ const AuthSelect = ({
                             style={{ margin: '0' }}
                         >
                             {showColorIcon &&
-                                options.find((option: any) => option.value === value).colors.split(' ').map((elem: string) => {
+                                options.find((option: any) => option.value === value).colors.split(' ').map((elem: string, idx: number) => {
                                     return (
                                         <div
+                                            key={idx}
                                             style={{
                                                 width: '16px',
                                                 height: '16px',
@@ -89,9 +90,10 @@ const AuthSelect = ({
                                             className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-100"
                                         >
                                             {showColorIcon &&
-                                                option.colors.split(' ').map((elem: string) => {
+                                                option.colors.split(' ').map((elem: string, idx: number) => {
                                                     return (
                                                         <div
+                                                            key={idx}
                                                             style={{
                                                                 width: '16px',
                                                                 height: '16px',
