42561bb88ed0d2cd9b66a9ac7b9709b137f6665a
Renan Vieira <r.souza06@hotmail.com>
2025-08-28 04:55:48 -0300

    fix color on toggle

--- src/components/ConfigSection/ConfigSection.tsx
+++ src/components/ConfigSection/ConfigSection.tsx
@@ -38,7 +38,7 @@ export const ConfigSection: React.FC<ConfigSectionProps> = ({
                             checked={isEnabled}
                             onChange={(e) => onToggleChange(e.target.checked)}
                         />
-                        <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+                        <div className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#7DC143] dark:peer-checked:bg-[#7DC143]"></div>
                         <span className="ms-3 text-sm font-medium">{label}</span>
                     </label>
                 </div>
