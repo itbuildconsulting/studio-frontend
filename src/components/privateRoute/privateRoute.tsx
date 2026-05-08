31422fd974edea1f481a9bb14a27c028639d40d2
Richard Salles <richard.danielcs@gmail.com>
2026-05-07 22:00:03 -0300

    FIX: Ajuste no Login

--- src/components/privateRoute/privateRoute.tsx
+++ src/components/privateRoute/privateRoute.tsx
@@ -11,8 +11,9 @@ interface PrivateRouteProps {
 }
 
 interface DecodedToken {
-    userId: number;
-    level: string;
+    id: number;
+    name: string;
+    employee_level: string;
     exp: number;
 }
 
@@ -36,10 +37,8 @@ export const PrivateRoute = ({ children }: PrivateRouteProps) => {
                 // Decodifica o token
                 const decoded: DecodedToken = jwtDecode(token);
 
-                // Verifica o nível do usuário
-                const userLevel = decoded.level;
+                const userLevel = decoded.employee_level;
 
-                // Exemplo de restrição de acesso: impedir que um funcionário acesse qualquer página, exceto a de aula
                 if (userLevel === '2' && !pathname.startsWith('/aula')) {
                     router.push('/aulas');
                     return;
